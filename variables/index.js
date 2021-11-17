b4cdedb572f9b1418b77b4db2bca3f1ea3da00d9
Higor Diego <higordiegoti@gmail.com>
2021-11-17 15:48:05 -0300

    fix: readme.md

---
+++ variables/index.js
@@ -0,0 +1,48 @@
+// main
+// console
+// console.warn('meu primeiro console.warn.')
+// console.info('meu primeiro console.info.')
+// console.log('meu primeiro console.log.')
+// console.error('meu primeiro console.error.')
+// variables
+
+// var | let | const
+
+// var - contexto global - variável global
+// let - contexto por escopo - variável de escopo
+// const - é uma variável constante que não muda.
+
+let nome = "Bruna" // string
+let profissao = "QA" // string
+let salario = "17000.00" // string
+
+// concatenação de string
+// console.log("nome: " + nome + " profissao: "+ profissao + " salario:", salario)
+// template string
+// console.log(`nome: ${nome} profissao: ${profissao} salario: ${salario}`)
+
+// console.log separado por string
+// console.log(nome, profissao, salario)
+
+/*
+    tipos: number, string, (object, array), boolean, undefined
+ */
+
+
+
+
+const minhaString = '[]' // string
+const meuNumber = 20 // number
+const meuObject = {name: 'higor'} // object
+const meuUndefined = 'testando' // string
+const meuArray = [1,2,3,4] // object
+
+// meuNumber = 50 // number
+// meuObject = 'higor diego' // string
+// meuUndefined = 29 // number
+
+meuObject.name = 'Bruna'
+
+// object
+// alterar o seu valor mas não o seu tipo
+console.log(meuObject)
